b8cb797a90fc3bee88f7037f687134fa4232ee56
ProjextSoft-STUDIONIONS <projectsoft2009@yandex.ru>
2026-02-17 01:08:21 +0400

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,16 +11,93 @@ module.exports = function(grunt) {
 		pkg : {},
 		clean: {
 			zip: ['*.zip'],
-			temp: ['temp']
+			temp: ['temp'],
 		},
-		requirejs: {},
-		uglify: {},
-		compress: {},
+		requirejs: {
+			main: {
+				options: {
+					baseUrl: "src/js/",
+					out: "plugins/dataattributes/plugin.js",
+					paths: {},
+					wrap: true,
+					skipModuleInsertion: true,
+					optimize: "none",
+					include: [
+						"plugin.js",
+					],
+					done: function(done, output) {
+						grunt.log.writeln(output.magenta);
+						grunt.log.writeln("Build ".cyan + "done!\n");
+						done();
+					},
+					error: function(done, err) {
+						grunt.log.warn(err);
+						done();
+					},
+				},
+			},
+		},
+		uglify: {
+			options: {
+				sourceMap: false,
+				banner: ``,
+				compress: {
+					drop_console: false
+				},
+				output: {
+					ascii_only: true
+				},
+			},
+			main: {
+				files: [
+					{
+						expand: true,
+						flatten : true,
+						src: [
+							"plugins/dataattributes/plugin.js"
+						],
+						dest: "plugins/dataattributes",
+						filter: "isFile",
+						rename: function (dst, src) {
+							return dst + "/" + src.replace(".js", ".min.js");
+						}
+					},
+				],
+			},
+		},
+		compress: {
+			main: {
+				options: {
+					archive: `dataattributes.zip`,
+				},
+				files: [
+					{
+						src: [
+							'plugins/**',
+						],
+						dest: `tinymce/`,
+					},
+				],
+			},
+			evo4: {
+				options: {
+					archive: `dataattributes-evo4.zip`,
+				},
+				files: [
+					{
+						src: [
+							'plugins/**',
+						],
+						dest: `dataattributes/assets/plugins/tinymce4/tinymce/`,
+					},
+				],
+			}
+		},
 	});
 	grunt.registerTask('default',	[
 		"clean",
 		"requirejs",
 		"uglify",
-		"compress"
+		"compress",
 	]);
 }
